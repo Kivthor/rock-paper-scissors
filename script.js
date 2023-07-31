4bb21aa1c198fe5fd3313d3effd5f521e571d92e
Kivthor <50101494+Kivthor@users.noreply.github.com>
2023-07-31 16:43:53 +0700

    избавился от глобальных переменных, кроме двух счетчиков

--- script.js
+++ script.js
@@ -1,54 +1,72 @@
-'use scrict'
+"use scrict";
 
-// Begin with a function called getComputerSelection that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’:
+// основная функция //////////////////////////////////////////////////////////////////////////////////////
 
-let computerSelection;
+let playerWinCounter = 0;
+let computerWinCounter = 0;
+
+function game() {
+  const numberOfRounds = getNumberOfRounds();
+
+  if (numberOfRounds) {
+    for (let i = 1; i <= numberOfRounds; i++) {
+
+      let playerSelection = getPlayerSelection();
+      if (playerSelection) {
+        alert(`You chose ${playerSelection}`);
+      } else {
+        return;
+      }
+
+      let computerSelection = getComputerSelection();
+      alert(`Computer chose ${computerSelection}`);
+
+      let roundResult = getRoundResult(playerSelection, computerSelection);
+      alert(roundResult);
+
+      roundResultCount(roundResult, playerSelection, computerSelection);
+    }
+
+    let gameResult = getGameResult(playerWinCounter, computerWinCounter);
+    return gameResult;
+
+  } else {
+    alert("Goodbye!\n \nGame is Over!");
+    return;
+  }
+}
+
+if (game()) {
+  alert(
+    `${getGameResult(
+      playerWinCounter,
+      computerWinCounter
+    )}\n \nYour score is --- (${playerWinCounter})\nComputer score is --- (${computerWinCounter})`
+  );
+}
+
+// вспомогательные функции ////////////////////////////////////////////////////////////////////////////////////
+
+function getRandomNumber0_2() {
+  // возвращает случайное число от 0 до 2
+  let randomNumber = Math.floor(Math.random() * 3);
+  return randomNumber;
+}
 
 function getComputerSelection() {
   // функция для вывода компьтером нужных значений случайным образом
-  computerSelection = Math.floor(Math.random() * 3); // выводит случайное число от 0 до 2
-
-  switch (computerSelection) {
+  switch (getRandomNumber0_2()) {
     case 0:
-      computerSelection = "Rock";
-      break;
+      return "Rock";
     case 1:
-      computerSelection = "Paper";
-      break;
+      return "Paper";
     case 2:
-      computerSelection = "Scissors";
-      break;
+      return "Scissors";
   }
-
-  return computerSelection;
 }
 
-// Writing a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-
-let playerSelection;
-let roundResult;
-function gameRound(playerSelection, computerSelection) {
-  // функция которая определяет победителя одного раунда игры (или ничью)
-
-  if (
-    (playerSelection === "Rock" && computerSelection === "Rock") ||
-    (playerSelection === "Paper" && computerSelection === "Paper") ||
-    (playerSelection === "Scissors" && computerSelection === "Scissors")
-  ) {
-    return (roundResult = `DRAW!\n \n${playerSelection} equals ${computerSelection}!`); // проверка на ничью
-  } else if (
-    (playerSelection === "Rock" && computerSelection === "Paper") ||
-    (playerSelection === "Paper" && computerSelection === "Scissors") ||
-    (playerSelection === "Scissors" && computerSelection === "Rock")
-  ) {
-    return (roundResult = `YOU LOSE!\n \n${computerSelection} beats ${playerSelection}!`); // проверка на проигрыш
-  } else
-    return (roundResult = `YOU WIN!\n \n${playerSelection} beats ${computerSelection}!`); // проверка на выигрыш
-}
-
-function firstCharOfStringInUpperCaseAndRestOfStringInLower() {
+function firstCharOfStringInUpperCaseAndRestOfStringInLower(playerSelection) {
   // функция берет строку и делает первую букву заглавной а остальные строчные (имя тупое, да)
-
   const stringLowerCase = playerSelection.toLowerCase(); // rock
   const stringFirstChar = stringLowerCase.charAt(0); // r
   const stringFirstCharUpperCase = stringFirstChar.toUpperCase(); // R
@@ -60,15 +78,17 @@ function firstCharOfStringInUpperCaseAndRestOfStringInLower() {
 
 function getPlayerSelection() {
   // функция для выбора значений которые вводит игрок
+  let playerSelection = prompt(
+    "Enter your choice: ",
+    "Rock, Paper or Scissors"
+  );
 
-  playerSelection = prompt("Enter your choice: ", "Rock, Paper or Scissors");
-
-  if (playerSelection === null || playerSelection === "") {
+  if (!playerSelection) {
     // проверка если игрок нажмет отмена или отправит пустое поле
-    alert("Goodbye!");
+    alert("Goodbye!\n \nGame is Over!");
     return;
   } else {
-    firstCharOfStringInUpperCaseAndRestOfStringInLower();
+    playerSelection = firstCharOfStringInUpperCaseAndRestOfStringInLower(playerSelection);
 
     if (
       playerSelection == "Rock" ||
@@ -78,16 +98,31 @@ function getPlayerSelection() {
       // проверка что вводятся именно нужные значения
       return playerSelection;
     } else {
-      alert("Wrong Choice!");
+      alert("Wrong Choice!\n \nGame is Over!");
       return;
     }
   }
 }
 
-let playerWinCounter = 0;
-let computerWinCounter = 0;
+function getRoundResult(playerSelection, computerSelection) {
+  // функция которая определяет победителя одного раунда игры (или ничью)
 
-function roundResultCount() {
+  if (
+    (playerSelection === "Rock" && computerSelection === "Rock") ||
+    (playerSelection === "Paper" && computerSelection === "Paper") ||
+    (playerSelection === "Scissors" && computerSelection === "Scissors")
+  ) {
+    return `DRAW!\n \n${playerSelection} equals ${computerSelection}!`; // проверка на ничью
+  } else if (
+    (playerSelection === "Rock" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Rock")
+  ) {
+    return `YOU LOSE!\n \n${computerSelection} beats ${playerSelection}!`; // проверка на проигрыш
+  } else return `YOU WIN!\n \n${playerSelection} beats ${computerSelection}!`; // проверка на выигрыш
+}
+
+function roundResultCount(roundResult, playerSelection, computerSelection) {
   // функция для подсчета побед игрока и компьютера в раунде
   switch (roundResult) {
     case `YOU WIN!\n \n${playerSelection} beats ${computerSelection}!`:
@@ -99,55 +134,19 @@ function roundResultCount() {
   }
 }
 
-let numberOfRounds;
-
 function getNumberOfRounds() {
-  // функция для ввода количства желаемых игроком раундов
-  numberOfRounds = +prompt("How many rounds you want to play?", 1);
+  // функция для ввода количества желаемых игроком раундов
+  let numberOfRounds = +prompt("How many rounds you want to play?", 1);
   return numberOfRounds;
 }
 
-let gameResult;
-
-function getGameResult() {
+function getGameResult(playerWinCounter, computerWinCounter) {
   // функция для подсчета количества очков в конце игры
   if (playerWinCounter === computerWinCounter) {
-    gameResult = "Game is finished!\n \nIT IS DRAW!";
+    return "Game is finished!\n \nIT IS DRAW!";
   } else if (playerWinCounter > computerWinCounter) {
-    gameResult = "Game is finished!\n \nYOU WON!";
+    return "Game is finished!\n \nYOU WON!";
   } else {
-    gameResult = "Game is finished!\n \nYOU LOST!";
+    return "Game is finished!\n \nYOU LOST!";
   }
-}
-
-function game() {
-  // функция для основной игры
-
-  if (getNumberOfRounds()) {
-    for (let i = 1; i <= numberOfRounds; i++) {
-      if (getPlayerSelection()) {
-        alert(`You chose the ${playerSelection}!`);
-      } else return;
-
-      getComputerSelection();
-      alert(`Computer chose the ${computerSelection}!`);
-
-      gameRound(playerSelection, computerSelection);
-      alert(roundResult);
-
-      roundResultCount();
-    }
-
-    getGameResult();
-    return gameResult;
-  } else {
-    alert("Goodbye!");
-    return;
-  }
-}
-
-if (game()) {
-  alert(
-    `${gameResult}\n \nYour score is --- (${playerWinCounter})\nComputer score is --- (${computerWinCounter})`
-  );
 }
